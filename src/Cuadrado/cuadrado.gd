151738f3d316a133855d0a6367ff7c8d350e4ae7
stchigel <Alumno26.STCHIGEL.GRIMALDI.Nicolas@ipm.edu.ar>
2026-04-14 16:06:27 -0300

    Mecánica base del juego completada: grilla de bloques activables/desactivables, generación de niveles dinámicamente desde código y físicas de agua

---
+++ src/Cuadrado/cuadrado.gd
@@ -0,0 +1,25 @@
+extends StaticBody2D
+@export var activo: bool
+
+func _ready() -> void:
+	input_pickable = true
+	cambiarModo()
+
+func cambiarModo():
+	if (!activo):
+		$CollisionShape2D.disabled = true
+		$Lleno.hide()
+		$Vacio.show()
+	else:
+		$CollisionShape2D.disabled = false
+		$Vacio.hide()
+		$Lleno.show()
+
+func _process(delta: float) -> void:
+	pass
+
+func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.pressed:
+		if activo: activo=false
+		else: activo=true
+		cambiarModo()
---
+++ src/Game/game.gd
@@ -0,0 +1,36 @@
+extends Node2D
+
+@export var celda_scene: PackedScene
+@onready var anchor := $GridMiddle
+var pointer: bool = true
+var attrForce: float = 500
+
+var mapa = [
+	[1, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+	[1, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+	[1, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+	[1, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+	[1, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+	[1, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+	[1, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+	[1, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+	[1, 0, 0, 0, 0, 0, 1, 1, 1, 1],
+	[1, 1, 1, 1, 1, 0, 1, 1, 1, 1]
+]
+
+const GRID_SIZE := 10
+const CELL_SIZE := 60
+
+func _ready():
+	generar_grilla()
+
+func generar_grilla():
+	var total_size = GRID_SIZE * CELL_SIZE
+	var half_size = total_size / 2
+	for x in GRID_SIZE:
+		for y in GRID_SIZE:
+			var celda = celda_scene.instantiate()
+			if mapa[y][x] == 0: 
+				celda.activo = false
+			celda.position = Vector2(x * CELL_SIZE - half_size, y * CELL_SIZE - half_size)
+			anchor.add_child(celda)
